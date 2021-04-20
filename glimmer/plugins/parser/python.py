1893f83b25cc86e563f756bcaa2f5050e8938131
WAY29 <1162410187@qq.com>
2021-04-20 11:47:59 +0800

    🐞fix(plugins/parser/python): check error if file not exist

--- glimmer/plugins/parser/python.py
+++ glimmer/plugins/parser/python.py
@@ -1,12 +1,11 @@
-from api import PluginParserBase, register_plugin, catch_stdout
-from os import path
+from api import PluginParserBase, register_plugin, catch_stdout, is_valid_pathname
 
 
 class Plugin(PluginParserBase):
     protocols = ["python"]
 
     def rule_check(self, module_path):
-        return self.protocol_check(module_path) and path.isfile(self.remove_protocol(module_path))
+        return self.protocol_check(module_path) and is_valid_pathname(self.remove_protocol(module_path))
 
     def get_data(self, module_path):
         try:
