734a5140a308ced9b0867904887881599007f905
WAY29 <1162410187@qq.com>
2021-04-21 07:38:39 +0800

    ✨feat(parser/python plugin README): parser/python pythons protocol

--- glimmer/plugins/parser/python.py
+++ glimmer/plugins/parser/python.py
@@ -2,17 +2,24 @@ from api import PluginParserBase, register_plugin, catch_stdout, is_valid_pathna
 
 
 class Plugin(PluginParserBase):
-    protocols = ["python"]
+    protocols = ["python", "pythons"]
 
     def rule_check(self, module_path):
         return self.protocol_check(module_path) and is_valid_pathname(self.remove_protocol(module_path))
 
     def get_data(self, module_path):
+        protocol = self.get_protocol(module_path)
+
         try:
             with open(self.remove_protocol(module_path), "r") as f, catch_stdout() as s:
                 data = f.read()
                 exec(data)
-            return (s.getvalue().strip(), )
+            result = s.getvalue().strip()
+            if protocol == "pythons":
+                result = result.split()
+            else:
+                result = (result, )
+            return result
         except Exception:
             return None
 
